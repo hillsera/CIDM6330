c3c436420fda798f77b19c2d0962a3cef1716403
Sera Hill <sahill1@buffs.wtamu.edu>
2024-02-20 16:46:43 -0600

    completed until challenge yourself section in tutorial part six

--- Assignment-2/django_projects/locallibrary/catalog/views.py
+++ Assignment-2/django_projects/locallibrary/catalog/views.py
@@ -32,3 +32,12 @@ def index(request):
 
     # render html template index.html with data in context var
     return render(request, 'index.html', context=context)
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+    
+class BookDetailView(generic.DetailView):
+    model = Book
