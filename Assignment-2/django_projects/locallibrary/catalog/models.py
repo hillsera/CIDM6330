aaeb2d811aa3735179207d5826258988149bf733
Sera Hill <sahill1@buffs.wtamu.edu>
2024-02-21 10:45:41 -0600

    completed tutorial part 8

--- Assignment-2/django_projects/locallibrary/catalog/models.py
+++ Assignment-2/django_projects/locallibrary/catalog/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.conf import settings
+from datetime import date
+
 
 # Create your models here.
 from django.urls import reverse
@@ -59,6 +62,14 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
+
+    @property
+    def is_overdue(self):
+        """Determines if the book is overdue based on due date and current date."""
+        return bool(self.due_back and date.today() > self.due_back)
+
+
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -77,6 +88,7 @@ class BookInstance(models.Model):
 
     class Meta:
         ordering = ['due_back']
+        permissions = (("can_mark_returned", "Set book as returned"),)
 
     def __str__(self):
         """String for representing the Model object."""
